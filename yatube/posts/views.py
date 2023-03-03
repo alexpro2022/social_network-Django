02b017e1d1fdd0bba87cf7e8a4207ba1ef959f54
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-03-03 22:43:36 +0300

    Updated tech stack

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
-
 from posts.forms import CommentForm, PostForm
 from posts.models import Follow, Group, GroupFollow, Post, User
+
 from yatube.settings import POSTS_PER_PAGE
 
 

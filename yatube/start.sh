c16c285933c5cf46ed562a82141812c41114bdcc
Alex Proskuriakov <alexpro1972@yandex.ru>
2023-04-24 17:12:39 +0300

    Added superuser creation

--- yatube/start.sh
+++ yatube/start.sh
@@ -4,4 +4,12 @@ python manage.py makemigrations
 python manage.py migrate
 # python manage.py load_all_data  #to do a load data of existing pics and posts
 python manage.py collectstatic --no-input
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+    python manage.py createsuperuser \
+        --no-input \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+
 gunicorn yatube.wsgi:application --bind 0:8000

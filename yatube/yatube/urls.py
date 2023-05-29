9569576332dc2fa8aa021955fb7f9d618a7c3b35
Aleksei Proskuriakov <alexpro1972@yandex.ru>
2023-05-29 22:53:36 +0300

    Update urls.py

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -16,5 +16,6 @@ handler500 = 'core.views.server_error'
 
 if settings.DEBUG:
     urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)    
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
